598013c1e5a87c885bb5785dd66a3d6dadded61d
Antonio Tsai <jimmyjack7777@gmail.com>
2016-12-06 01:56:36 +0800

    Add pickup & carry feature from tutorial

---
+++ Assets/PickupObject.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickupObject : MonoBehaviour {
+	GameObject mainCamera;
+	bool carrying;
+	GameObject carriedObject;
+	public float distance;
+	public float smooth;
+
+	// Use this for initialization
+	void Start () {
+		mainCamera = GameObject.FindWithTag ("MainCamera");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (carrying) {
+			carry (carriedObject);
+			checkDrop ();
+		} else {
+			pickup ();
+		}
+	}
+
+	void carry(GameObject o) {
+		// Use Vector3.Lerp to move smoothly
+		//o.transform.position = mainCamera.transform.position + mainCamera.transform.forward * distance;
+		o.transform.position = Vector3.Lerp(o.transform.position, mainCamera.transform.position + mainCamera.transform.forward * distance, Time.deltaTime * smooth);
+	}
+
+	void checkDrop() {
+		if (Input.GetKeyDown (KeyCode.E)) {
+			dropObject ();
+		}
+	}
+
+	void dropObject() {
+		carrying = false;
+		carriedObject.GetComponent<Rigidbody>().isKinematic = false;
+		carriedObject = null;
+	}
+
+	void pickup() {
+		if (Input.GetKeyDown (KeyCode.E)) {
+			int x = Screen.width / 2;
+			int y = Screen.height / 2;
+
+			Ray ray = mainCamera.GetComponent<Camera>().ScreenPointToRay (new Vector3 (x, y));
+			RaycastHit hit;
+			if(Physics.Raycast(ray, out hit)) {
+				Pickupable p = hit.collider.GetComponent<Pickupable>();
+				if(p != null) {
+					carrying = true;
+					carriedObject = p.gameObject;
+					p.gameObject.GetComponent<Rigidbody>().isKinematic = true;
+				}
+			}
+		}
+	}
+}
