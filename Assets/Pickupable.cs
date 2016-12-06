0069d125c3a07029d7be976def86a30f121ca260
Antonio Tsai <jimmyjack7777@gmail.com>
2016-12-06 09:17:03 +0800

    Enable gaze on object which is pickable

--- Assets/Pickupable.cs
+++ Assets/Pickupable.cs
@@ -1,15 +1,45 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Pickupable : MonoBehaviour {
+public class Pickupable : MonoBehaviour, IGvrGazeResponder {
+	// GvrGaze & Mindwave
+	mind mindObject;
+	int blink = 0;
+	public bool isLookingAt = false;
+	public bool isOpen = false;
 
 	// Use this for initialization
 	void Start () {
-	
+		// Mindwave
+		mindObject = GameObject.Find ("Mind").GetComponent<mind> ();
+		blink = mindObject.Blink;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (this.isLookingAt) {
+			if (mindObject.Blink != blink)
+			{
+				
+			}
+			blink = mindObject.Blink;
+		}
+	}
+
+	public void setGazeAt(bool lookingAt){
+		isLookingAt = lookingAt;
+	}
+
+
+	public void OnGazeEnter(){
+		setGazeAt (true);
+		blink = mindObject.Blink;
+	}
+
+	public void OnGazeExit(){
+		setGazeAt (false);
+	}
+
+	public void OnGazeTrigger() {
 	}
 }
