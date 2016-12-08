84dc20d03c3a310b19800e9898f0d3216a220243
JasonPan <jason8399@gmail.com>
2016-12-09 02:00:16 +0800

    Finish ChangePic.cs and add it to first TV.

--- Assets/ChangePic.cs
+++ Assets/ChangePic.cs
@@ -1,15 +1,51 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
-public class ChangePic : MonoBehaviour {
+public class ChangePic : MonoBehaviour, IGvrGazeResponder {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
+    private SpriteRenderer spriteRenderer;
+    mind mindObject;
+    int blink = 0;
+    public bool isLookingAt = false;
+    public bool isOpen = false;
+
+    // Use this for initialization
+    void Start () {
+        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        mindObject = GameObject.Find("Mind").GetComponent<mind>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        if(this.isLookingAt) {
+            if (mindObject.Blink != blink)
+            {
+                spriteRenderer.enabled = !this.isOpen;
+                this.isOpen = !this.isOpen;
+            }
+            blink = mindObject.Blink;
+        }
+    }
+
+    public void setGazeAt(bool lookingAt)
+    {
+        isLookingAt = lookingAt;
+    }
+
+    public void OnGazeEnter()
+    {
+        setGazeAt(true);
+        blink = mindObject.Blink;
+    }
+
+    public void OnGazeExit()
+    {
+        setGazeAt(false);
+    }
+
+    public void OnGazeTrigger()
+    {
+        throw new NotImplementedException();
+    }
 }
