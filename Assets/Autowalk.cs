c54c48d90fc52bc1871dc0196b535bb8fe47683d
Antonio Tsai <jimmyjack7777@gmail.com>
2016-12-16 13:36:15 +0800

    Change walking speed

--- Assets/Autowalk.cs
+++ Assets/Autowalk.cs
@@ -5,9 +5,11 @@ using UnityStandardAssets.Characters.FirstPerson;
 public class Autowalk : MonoBehaviour {
 	private FirstPersonController controller;
 	private mind neuroSky;
+	public float speed;
 
 	// Use this for initialization
 	void Start () {
+		speed = 1.2f;
 		controller = GameObject.FindObjectOfType<FirstPersonController> ();
 		neuroSky = GameObject.Find ("Mind").GetComponent<mind> ();
 	}
@@ -17,7 +19,7 @@ public class Autowalk : MonoBehaviour {
 
 		if (neuroSky.poorSignal == 0 && neuroSky.atten > 50 ) {
 			// Speed Up
-			controller.M_WalkSpeed = 2;
+			controller.M_WalkSpeed = speed;
 		} else {
 			//else if(neuroSky.poorSignal == 0 && neuroSky.atten <= 50){
 			// Slow Down
